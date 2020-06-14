f6d464a21a7a66e488d6172f15470990062a7dd3
Roberto Vega <55059546+peruviantorch@users.noreply.github.com>
2020-06-13 21:12:36 -0500

    repositorio Serie findByNombre

--- NotSpoiled/src/main/java/Kaori/wiki/repositorios/Serie_Repositorio.java
+++ NotSpoiled/src/main/java/Kaori/wiki/repositorios/Serie_Repositorio.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import Kaori.wiki.entidades.Serie;
 
 public interface Serie_Repositorio extends CrudRepository<Serie, String>{
-
+  Serie findByNombre(String nombre);
 }
