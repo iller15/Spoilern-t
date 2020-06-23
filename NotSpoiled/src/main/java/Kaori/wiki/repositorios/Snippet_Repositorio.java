9506bd9df69ccc5b5978a690b8d2e4cb661a2ca7
Roberto Vega <55059546+peruviantorch@users.noreply.github.com>
2020-06-23 12:46:21 -0500

    Update Snippet_Repositorio.java

--- NotSpoiled/src/main/java/Kaori/wiki/repositorios/Snippet_Repositorio.java
+++ NotSpoiled/src/main/java/Kaori/wiki/repositorios/Snippet_Repositorio.java
@@ -9,4 +9,5 @@ import Kaori.wiki.entidades.Snippet;
 public interface Snippet_Repositorio extends CrudRepository<Snippet, String>{
 	@Override
 	List<Snippet> findAll();
+	List<Snippet> findAllByCapitulo_idCapitulo(String idCapitulo);
 }
